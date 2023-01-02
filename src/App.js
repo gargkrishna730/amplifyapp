becff1340cce4fd7f8940d59ce47d003ba98afe8
Krishna Garg <gargkrishna730@gmail.com>
2023-01-03 01:45:07 +0530

    fourth commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h1> Hello Souad! You will be fine don't worry</h1>.
+          <h1> Hello Souad! You will be fine don't worry</h1>
           <h1>Krishna is here with you</h1>
           <h1>Krishna Loves You❤</h1>
           <h1> I Love You❤</h1>
